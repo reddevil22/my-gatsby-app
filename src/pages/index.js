79947b53770b0c5946c2d8e3f8efaaae1a0b95f1
Joel Fredericks <joel.fredericks@kurtosys.com>
2019-02-17 20:44:13 +0200

    Added logic to create blog posts using nodes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,55 @@
 import React from "react";
-
+import { graphql } from 'gatsby';
 import Layout from "../components/layout";
-import Image from "../components/image";
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import SEO from "../components/seo";
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
 	<Layout>
 		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-		<h1>Hi Nicole</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
 		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
+			{/* <Image /> */}
+			<h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+			{data.allMarkdownRemark.edges.map(({ node }) => (
+				<div key={node.id}>
+					<h3
+						css={css`
+						margin-bottom: ${rhythm(1 / 4)};
+						`}
+					>
+						{node.frontmatter.title}{" "}
+						<span
+							css={css`
+								color: #bbb;
+							`}
+						>
+							— {node.frontmatter.date}
+						</span>
+					</h3>
+					<p>{node.excerpt}</p>
+				</div>
+			))}
 		</div>
 	</Layout>
 );
+
+export const query = graphql`
+	query {
+		allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date(formatString: "DD MMMM, YYYY")
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`
 
 export default IndexPage;
